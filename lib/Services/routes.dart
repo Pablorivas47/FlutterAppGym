149fcee4491db67d549d6748735676cabc66b6cc
Pablorivas47 <rivaspablo1172000@gmail.com>
2024-08-17 17:52:29 -0300

    change

--- lib/Services/routes.dart
+++ lib/Services/routes.dart
@@ -16,8 +16,6 @@ import 'package:flutter_application_1/api/google-maps.dart';
 import 'package:flutter_application_1/components/custom_navigation_bar.dart';
 import 'package:flutter_application_1/components/custom_bottom_navigation_bar.dart';
 import 'package:flutter_application_1/components/custom_n_b.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 export 'package:flutter_application_1/Screens/page_404.dart';
 export 'package:flutter_application_1/Screens/routine.dart';
 export 'package:flutter_application_1/Screens/profile.dart';
